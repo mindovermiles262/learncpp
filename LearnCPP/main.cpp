77840174ed1b8eb8c579d0e719743eabef96406a
Andy D <mindovermiles262@gmail>
2019-03-21 15:09:43 -0700

    Initial Commit

---
+++ LearnCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCPP
+//
+//  Created by Andrew Duss on 3/21/19.
+//  Copyright © 2019 Andrew Duss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
